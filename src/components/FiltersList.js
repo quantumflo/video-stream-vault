df6397d2caabad080306b79cd77adc60c3e7b134
shashank <shashankhebbarm@gmail.com>
2023-12-22 18:18:48 +0530

    Refactor FiltersList component and add VideoCard component

--- src/components/FiltersList.js
+++ src/components/FiltersList.js
@@ -1,12 +1,45 @@
 import FilterButton from "./FilterButton"
+import React, { useRef, useState } from 'react';
 
 const FiltersList = () => {
-  const filters = ["Live", "Past", "Upcoming", "Sports", "Gaming", "Movies", "Music", "News", "Fashion", "Learning", "Finance", "Politics", "Science & Tech", "Travel & Outdoors", "Video Games", "Other"]
+  const filters = ["Live", "Past", "Upcoming", "Sports", "Gaming", "Movies", "Music", "News", "Fashion", "Learning", "Finance", "Politics", "Science", "Travel", "Video", "Other"]
+  const containerRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = (direction) => {
+    const container = containerRef.current;
+    const scrollAmount = 400; // Adjust the scroll amount as needed
+
+    if (direction === "left") {
+      container.scrollLeft -= scrollAmount;
+      setScrollPosition(container.scrollLeft);
+    } else if (direction === "right") {
+      container.scrollLeft += scrollAmount;
+      setScrollPosition(container.scrollLeft);
+    }
+  };
+
+  const scrollButtonCss = "bg-gray-600 text-white px-2 font-bold";
+  
   return (
-    <div className="flex overflow-x-scroll">
-      {filters.map((filter, index) => (
-        <FilterButton key={index} name={filter}></FilterButton>
-      ))}
+    <div className="flex overflow-x-hidden ">
+      <button
+        className={scrollButtonCss}
+        onClick={() => handleScroll("left")}
+      >
+        {"<"}
+      </button>
+      <div className="flex flex-nowrap h-16 mx-5 overflow-hidden" ref={containerRef}>
+        {filters.map((filter, index) => (
+          <FilterButton key={index} name={filter}></FilterButton>
+        ))}
+      </div>
+      <button
+        className={scrollButtonCss}
+        onClick={() => handleScroll("right")}
+      >
+        {">"}
+      </button>
     </div>
   )
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,8 +3,16 @@
 const VideoCard = ({info}) => {
 const {snippet, statistics} = info;
 const {channelTitle, title, description, thumbnails} = snippet;
+console.log(info)
   return (
-    <div>VideoCard</div>
+    <div className=" m-4 w-80">
+      <img className="rounded-lg max-w-110" src={thumbnails.medium.url} alt="thumbnail" />
+      <ul>
+        <li className="text-lg p-2 font-semibold line-clamp-2">{title}</li>
+        <li className="text-sm px-2">{channelTitle}</li>
+        <li className="text-sm px-2">{statistics.viewCount} views</li>
+      </ul>
+    </div>
   )
 }
 
