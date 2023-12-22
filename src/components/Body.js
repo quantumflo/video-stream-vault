091f1569a252e754cf9615d6063e1d7b21f2a4a6
shashank <shashankhebbarm@gmail.com>
2023-12-22 18:18:36 +0530

    Refactor Body component and update constants.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,10 @@
 import SideBar from "./SideBar";
-import MainContainer from "./MainContainer";
+import { Outlet } from "react-router-dom";
 const Body = () => {
   return (
     <div className="flex min-h-screen">
       <SideBar></SideBar>
-      <MainContainer></MainContainer>
+      <Outlet />
     </div>
   );
 };
