76e54249dabbfb09bc21d1ecfb994ab034f75bb0
shashank <shashankhebbarm@gmail.com>
2023-12-22 18:18:19 +0530

    Add react-router-dom and update dependencies

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Body from "./components/Body";
 import Header from "./components/Header";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchVideo from "./components/WatchVideo";
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <Body />,  children: [
+    {
+      path: "/",
+      element: <MainContainer />
+    },
+    {
+      path: "/watch",
+      element: <WatchVideo />
+    }
+  ] }
+]);
+
 function App() {
   return (
     <Provider store={store}>
       <div className="">
         <Header />
-        <Body />
+        <RouterProvider router={appRouter}>
+          <Body />
+        </RouterProvider>
       </div>
     </Provider>
   );
