39dd94bd3ab7d3cd264f7021194ed4a81adf6313
shashank <shashankhebbarm@gmail.com>
2023-12-22 23:00:57 +0530

    Update YouTube API key

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 export const MAINLOGOIMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg9RQaSsMRiyK8MzG0HIttvLqlxnM44a6V_V8f0tH4otaU5NcoBmx6VTz0i73w2sFMeyA&usqp=CAU";
 export const HAMBURGERIMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPm18L_L37fW_pi1VVymz7Ukq_SAf7R5E_jQ&usqp=CAU";
-// const REACT_APP_YOUTUBE_API_KEY = "AIzaSyBCUmXjJ10FoovnrgpIMlele_opCU5GvlU";
- const REACT_APP_YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const REACT_APP_YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=US&key="+ REACT_APP_YOUTUBE_API_KEY;
 export const PROFILEIMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUWXln2vaxONq5KB-FB-TYVqXGthR53CqiuNHL_kYYFhXBP9U8LGdT2vNFqM7nmgBuk9Q&usqp=CAU";
